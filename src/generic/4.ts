ee6434cfa39d58dd36ba6e709fb7ce22de5660aa
Maksym-Zyza <maksym.zyza@brickredsystems.ca>
2024-02-14 14:20:55 +0200

    added generic

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,14 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PageType {
+  title: string;
 }
-
-class Page extends Component {
-  pageInfo () {
+class Component<T extends PageType> {
+  constructor(public props: T) {}
+}
+class Page extends Component<PageType> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
